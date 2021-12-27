8796fb69d9abff0733b9e989709381c7bfd37324
nisarga7 <nisarga.xworks@gmail.com>
2021-12-27 12:16:12 +0530

    adding

---
+++ Biriyani.java
@@ -0,0 +1,11 @@
+class Biriyani
+{
+public static void main(String []args)
+{
+String months[]={"january","february","march","april","may","june","july","august","september","october","november","december"};
+for(int index=0;index<=11;index++)
+{
+System.out.println(months[index]);
+}
+}
+}
